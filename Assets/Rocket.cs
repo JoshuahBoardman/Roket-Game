6433a7681320128466768203782f6a206ef764d6
JoshuahBoardman <joshuahboardman@gmail.com>
2020-06-13 13:29:45 -0400

    Rocket movement was added

---
+++ Assets/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // Can thrust while rotating.
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+
+            print("Thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+            print("Rotating right");
+        }
+
+    }
+}
