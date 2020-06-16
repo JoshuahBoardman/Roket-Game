60fbbee75507ee610c332ca1f0617ba1cf60d4ae
JoshuahBoardman <joshuahboardman@gmail.com>
2020-06-16 15:38:49 -0400

    Added audio to the rocket

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -24,10 +28,17 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) // Can thrust while rotating.
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) // so it doesn't layer
+            {
+                audioSource.Play();
+            }
 
             print("Thrusting");
         }
-
+        else
+        {
+            audioSource.Stop();
+        }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
