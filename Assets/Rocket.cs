1a65a173b83d109e5c6d00f4a49b7f308327ebda
JoshuahBoardman <joshuahboardman@gmail.com>
2020-06-24 17:02:05 -0400

    Partical Effects and new sounds added

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,18 +1,24 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal.Profiling.Memory.Experimental;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
 
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rcsThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip finishSound;
 
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathSoundParticles;
+    [SerializeField] ParticleSystem finishSoundParticles;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending }
+    State state = Rocket.State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -25,44 +31,83 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            Rotate();
+        }
     }
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; } // ignore collisions when dead
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 // do nothing
-                print("Ok"); //todo remove
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                startSuccessSequence();
                 break;
             default:
-                print("Dead");
-                // kill player
+                startDeathSequence();
                 break;
         }
     }
-    private void Thrust()
+
+    private void startSuccessSequence()
     {
-        float thrustThisFrame = mainThrust * Time.deltaTime;
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(finishSound);
+        finishSoundParticles.Play();
+        Invoke("LoadNextLevel", 1f); // parameterise time
+    }
+
+    private void startDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathSound);
+        deathSoundParticles.Play();
+        Invoke("LoadFirstLevel", 1f); // parameterise time
+                                      // kill player
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    private void RespondToThrustInput()
+    {
         if (Input.GetKey(KeyCode.Space)) // Can thrust while rotating.
         {
-            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
-            if (!audioSource.isPlaying) // so it doesn't layer
-            {
-                audioSource.Play();
-            }
-
-            print("Thrusting");
+            ApplyThrust();
         }
         else
         {
             audioSource.Stop();
+            mainEngineParticles.Stop();
         }
     }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying) // so it doesn't layer
+        {
+            audioSource.PlayOneShot(mainEngine);
+            mainEngineParticles.Play();
+        }
+     
+    }
+
     private void Rotate()
     {
         rigidBody.freezeRotation = true; //take manual control of rotation
